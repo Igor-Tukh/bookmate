396c0a977aca4c655c11679ee43b81841f35f1d8
Igor Tukh <igor-tukh@yandex.ru>
2018-12-13 00:16:08 +0300

    Features builder and text extracting improved

--- src/doc2vec_module/src/build_features.py
+++ src/doc2vec_module/src/build_features.py
@@ -1,20 +1,96 @@
 from src.doc2vec_module.src.process_epub import get_text_by_session
 
+import nltk
+import string
+import csv
+
+
+USER_IDS = ['42607', '374866', '1433804', '1540818', '1855471', '1970134', '1997078', '2067903', '2309986',
+            '2488778', '2497291', '2504830', '2558482', '2694654', '2738651', '2750150', '2810724']
+
+PUNCTUATION = string.punctuation
+
 
 class FeaturesBuilder(object):
     def __init__(self, sessions):
         self.sessions = sessions
         self.features = []
+        self.text_features_builders = {'words_number': FeaturesBuilder.calculate_words_number,
+                                       'sentences_number': FeaturesBuilder.calculate_sentences_number,
+                                       'average_word_len': FeaturesBuilder.calculate_average_word_len,
+                                       'average_sentence_len': FeaturesBuilder.calculate_average_sentence_len}
 
-    def build_features(self):
+        self.text_features_list = [name for name, _ in self.text_features_builders.items()]
+
+    def build_features(self, certain_text_features=None, save_features=True, filename='features', clear_features=True):
+        if clear_features:
+            self.features = []
+
+        text_features = self.text_features_list if certain_text_features is None else certain_text_features
         for session in self.sessions:
-            self.features.append(FeaturesBuilder.build_features_for_session(session))
+            session_features = self.build_text_features_for_session(session)
+            session_features['session_id'] = session['_id']
+            self.features.append(session_features)
+            # for other session_features.update()
 
-    @staticmethod
-    def build_features_for_session(session):
+        if not save_features:
+            return
+
+        with open(filename + '.csv', 'w') as csv_file:
+            fields = text_features  # to other + ...
+            fields.append('session_id')
+            writer = csv.DictWriter(csv_file, fieldnames=fields)
+            writer.writeheader()
+
+            for session_features in self.features:
+                writer.writerow(session_features)
+
+    def upload_features(self, filename, clear_old=True):
+        if clear_old:
+            self.features = []
+
+        with open(filename) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                current_features = {}
+                for field in reader.fieldnames:
+                    current_features[field] = row[field]
+                self.features.append(current_features)
+
+    def build_text_features_for_session(self, session, certain_features_list=None):
         text = get_text_by_session(session, session['book_id'], session['document_id'])
-        return FeaturesBuilder.build_features_from_text(text)
+        words = nltk.word_tokenize(text)
+        running_features = self.text_features_list if certain_features_list is None else certain_features_list
+        current_features = {}
+        for feature in running_features:
+            current_features[feature] = self.text_features_builders[feature](words)
+        return current_features
 
     @staticmethod
-    def build_features_from_text(text):
-        pass
+    def calculate_words_number(words):
+        count = 0
+        for word in words:
+            if word not in PUNCTUATION:
+                count += 1
+        return count
+
+    @staticmethod
+    def calculate_sentences_number(words):
+        count = 0
+        for word in words:
+            if word == '.':
+                count += 1
+        return count
+
+    @staticmethod
+    def calculate_average_word_len(words):
+        return sum([len(word) for word in words]) / len(words)
+
+    @staticmethod
+    def calculate_average_sentence_len(words):
+        total_len = int(FeaturesBuilder.calculate_average_word_len(words) * len(words))
+        return total_len / FeaturesBuilder.calculate_sentences_number(words)
+
+
+if __name__ == '__main__':
+    pass
--- src/doc2vec_module/src/process_epub.py
+++ src/doc2vec_module/src/process_epub.py
@@ -2,9 +2,12 @@ from ebooklib import epub, ITEM_DOCUMENT, ITEM_UNKNOWN, ITEM_COVER
 from bs4 import BeautifulSoup
 from io import StringIO
 from pymongo import MongoClient
+from epub_conversion.utils import open_book, convert_epub_to_lines
+from xml_cleaner import to_raw_text
 
 import nltk
 import string
+import os
 
 punctuation = string.punctuation
 NUMBER_OF_BYTES_PER_CHARACTER = 2
@@ -133,15 +136,62 @@ def get_text_by_session(session, book_id, document_id):
     return session_text
 
 
+def get_epub_book_text_with_ebook_convert(book_id):
+    path = os.path.join('..', 'epub', book_id + '.epub')
+    output_path = os.path.join('..', 'txt_from_epub', book_id + '.txt')
+
+    if not os.path.isfile(output_path):
+        os.system("ebook-convert {path} {output_path}".format(path=path,
+                                                              output_path=output_path))
+
+    text = ''
+    with open(output_path, 'r') as file:
+        for line in file.readlines():
+            text += line
+
+    return text
+
+
+def get_epub_book_text_experimental(book_id):
+    folder = os.path.join('..', 'epub')
+
+    book = open_book(os.path.join(folder, '{book_id}.epub'.format(book_id=book_id)))
+    text = ''
+    for sentence in convert_lines_to_text(convert_epub_to_lines(book)):
+        for item in sentence:
+            text += item
+
+    return text
+
+
+def convert_lines_to_text(lines):
+    results = []
+    for line in lines[:10]:
+        sentence = to_raw_text(line)
+        print(sentence)
+        for item in sentence:
+            results.append(item)
+    return results
+
+
+def print_document_stats(book_id, document_id):
+    book_len = len(get_epub_book_text_with_ebook_convert(book_id))
+    items_len = get_items_summary_len(document_id)
+    print('Text len: {len}, items summary len: {items_len}, ratio: {ratio}'.format(len = book_len,
+                                                                                   items_len=items_len,
+                                                                                   ratio=items_len / book_len))
+
+
 if __name__ == '__main__':
-    book_ids = ['135089']
-    document_ids = ['1222472']
-    for book_id, document_id in zip(book_ids, document_ids):
-        print('Processing book {book_id} started'.format(book_id=book_id))
-        book_len = get_epub_book_len(book_id)
-        items_len = get_items_summary_len(document_id)
-        print('Text len: {len}, items summary len: {items_len}, ratio: {ratio}'.format(len = book_len,
-                                                                                       items_len=items_len,
-                                                                                       ratio=items_len / book_len))
-        # process_items(book_id, document_id)
-        # save_text_for_items(book_id, document_id)
+    print_document_stats('135089', '1222472')
+    # book_ids = ['135089']
+    # document_ids = ['1222472']
+    # for book_id, document_id in zip(book_ids, document_ids):
+    # print('Processing book {book_id} started'.format(book_id=book_id))
+    # book_len = get_epub_book_len(book_id)
+    # items_len = get_items_summary_len(document_id)
+    # print('Text len: {len}, items summary len: {items_len}, ratio: {ratio}'.format(len = book_len,
+    #                                                                                items_len=items_len,
+    #                                                                                ratio=items_len / book_len))
+    # process_items(book_id, document_id)
+    # save_text_for_items(book_id, document_id)
